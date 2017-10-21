025a2e73d5bfe79615c7fd066f127d01492d7350
klpk011 <klpk011@klpk011.local>
2017-10-21 11:26:39 +0800

    add order UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   devise_for :users
 
   resources :events
@@ -7,20 +6,23 @@ Rails.application.routes.draw do
   resource :user
 
   namespace :admin do
-    root "events#index"
+    root 'events#index'
     resources :events do
-      resources :tickets, :controller => "event_tickets"
-        collection do
-          post :bulk_update
-        end
+      resources :tickets, controller: 'event_tickets'
+      collection do
+        post :bulk_update
+      end
+
+      member do
+        post :reorder
+      end
     end
     resources :users do
-      resource :profile, :controller => "user_profiles"
+      resource :profile, controller: 'user_profiles'
     end
   end
 
-  root "events#index"
+  root 'events#index'
 
-  get "/faq" => "pages#faq"
-
+  get '/faq' => 'pages#faq'
 end
