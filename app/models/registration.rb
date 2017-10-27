e27205cfd24496dbfa7c7814c165bd5344aa35e9
klpk011 <klpk011@klpk011.local>
2017-10-27 19:16:56 +0800

    add validates for registration form

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -13,9 +13,22 @@ class Registration < ApplicationRecord
     self.uuid
   end
 
+  attr_accessor :current_step
+  validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
+  validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
+
   protected
 
   def generate_uuid
     self.uuid = SecureRandom.uuid
   end
+
+  def should_validate_basic_data?
+    current_step == 2
+  end
+
+  def should_validate_all_data?
+    current_step == 3 || status == "confirmed"
+  end
+
 end
